f1b858a4d4474a4c179619c38b0ac3d2af8ce97f
DanTheManGude <djg4755@rit.edu>
2018-01-03 17:58:29 -0500

    Started calculating avgs & basic formating

--- src/App.js
+++ src/App.js
@@ -22,31 +22,87 @@ class Grade extends Component {
         this.state = {
             name: "New Grade",
             level: this.props.level,
-            value: 95,
+            value: 100,
+            values: [],
             grades: [],
         };
     }
 
+    renderCalc() {
+        return (
+            <button className="btn btn-success btn-sm"
+                onClick={() => this.handleCalc()}>
+    			{'CALC average'}
+    		</button>
+		);
+    }
 
-    addGrade() {
-        const newGrade = <Grade level={this.state.level +1}/>;
+    handleCalc() {
+        console.log('starting handleCalc');
+
+        var avg = 0;
+
+        console.log(this.state.values);
+
+        for(var i in this.state.values) {
+            avg += this.state.values[i];
+        }
+
+        avg /= this.state.values.length;
+        this.setState({value: avg});
+        console.log('finished avg ' + avg);
+        console.log('finished handleCalc');
+    }
+
+    renderAdd() {
 		return (
-            <button className="btn btn-primary"
-                onClick={() => this.setState({grades: this.state.grades.concat(newGrade)})}>
+            <button className="btn btn-primary btn-sm"
+                onClick={() => this.handleAdd()}>
     			{'ADD Grade'}
     		</button>
 		);
-	}
+    }
+
+    handleAdd(){
+        console.log('starting handleAdd');
+        var newGrade = <Grade index={this.state.grades.length +1} parent={this} level={this.state.level +1}/>;
+        this.setState((prevState) => ({
+            grades: prevState.grades.concat([newGrade]),
+            values: prevState.values.concat([100]),
+        }));
+
+        this.handleCalc();
+
+        console.log('finished handleAdd');
+    }
+
+    renderChange(){
+        return (
+            <button className="btn btn-warning btn-sm"
+                onClick={() => this.handleChange()}>
+    			{'Change Value'}
+    		</button>
+		);
+    }
+
+    handleChange(){
+        this.setState({value: 55});
+        //this.props.parent.setState({values[this.props.index]: 0});
+    }
 
     render() {
-        const listItems = this.state.grades.map((grades) => <li>{grades}</li>);
+        var listItems = this.state.grades.map((grades) => <li>{grades}</li>);
+        console.log(this.state.values);
+        console.log(this.state.value);
         return (
             <div>
-                <p>{this.state.name}</p>
-                <div className="GradesListcontainer">
+                <p>{this.state.name}&nbsp;&nbsp;<strong>{this.state.value}</strong></p>
+                <div className="GradesList-container">
                     <ul>
                         {listItems}
-                        <li>{this.addGrade()}</li>
+                        <li>{this.renderChange()}</li>
+                        <li>{this.renderAdd()}</li>
+                        <li>{this.renderCalc()}</li>
                     </ul>
                 </div>
             </div>
