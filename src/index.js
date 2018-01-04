59025a86eb5954be0d75bdf06d8c97160d3842b9
DanTheManGude <djg4755@rit.edu>
2018-01-04 18:13:19 -0500

    Added Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const grade = (state = 0, action) => {
+    switch (action.type) {
+        case 'CHANGE':
+          return state + 1
+        default:
+          return state
+      }
+}
+
+const Grade = ({
+    value,
+    onChange
+}) => (
+    <div>
+      <p><strong>{value}</strong></p>
+      <ul>
+        <li><button className="btn btn-warning btn-sm" onClick={onChange}>Change</button></li>
+      </ul>
+    </div>
+)
+
+const store = createStore(grade);
+
+const render = () => {
+    ReactDOM.render(
+        <div className="app">
+          <p>
+            Work in progress, check back soon.
+          </p>
+          <div className="base-grade">
+              <Grade
+                value={store.getState()}
+                onChange={() =>
+                    store.dispatch({
+                        type: 'CHANGE'
+                    })
+                }
+              />
+          </div>
+        </div>,
+        document.getElementById('root')
+    );
+};
+
+store.subscribe(render);
+render();
+
 registerServiceWorker();
