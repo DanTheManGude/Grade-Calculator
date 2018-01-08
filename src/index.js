e1dcb4fdf3c1c3cc0241d221668ad379526683e4
DanTheManGude <djg4755@rit.edu>
2018-01-07 19:14:13 -0500

    Finished the reducers for adding grades

--- src/index.js
+++ src/index.js
@@ -1,31 +1,178 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import Component from 'react';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
+import { combineReducers } from 'redux';
 
-const grade = (state = 0, action) => {
+class GradeApp extends React.Component {
+    render() {
+        return(
+            <div>
+                <p>{this.props.state.baseData.name} <strong>{this.props.state.baseData.avg}</strong></p>
+                <ul>
+                    <li><button
+                        className="btn btn-warning btn-sm" onClick={() => {
+                            store.dispatch({
+                                type: 'MODIFY',
+                            })
+                        }}>Modify</button>
+                    </li>
+                    <li><button
+                        className="btn btn-primary btn-sm" onClick={() => {
+                            store.dispatch({
+                                type: 'MORE',
+                                h: []
+                            })
+                        }}>More</button>
+                    </li>
+                    {this.props.state.grades.map(grade =>
+                        <li key={grade.id}>
+                            <Grade state={grade}/>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        );
+    }
+}
+
+class Grade extends React.Component {
+    render() {
+        return(
+            <div>
+                <p>{this.props.state.name} <strong>{this.props.state.avg}</strong>
+                </p>
+                <ul>
+                    <li><button
+                        className="btn btn-primary btn-sm" onClick={() => {
+                            store.dispatch({
+                                type: 'ADD',
+                                h: this.props.state.heritage.concat(this.props.state.id)
+                            })
+                        }}>Add</button>
+                    </li>
+                    <li><button
+                        className="btn btn-warning btn-sm" onClick={() => {
+                            store.dispatch({
+                                type: 'CHANGE_NAME',
+                                name: 'New Name',
+                                h: this.props.state.heritage.concat(this.props.state.id)
+                            });
+                        }}>Change Name</button>
+                    </li>
+                    <li><button
+                        className="btn btn-info btn-sm" onClick={() => {
+                            store.dispatch({
+                                type: 'CHANGE_AVG',
+                                avg: 229,
+                                h: this.props.state.heritage.concat(this.props.state.id)
+                            });
+                        }}>Change Grade</button>
+                    </li>
+                    {this.props.state.grades.map(grade =>
+                        <li key={grade.id}>
+                            <Grade state={grade}/>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        );
+    }
+}
+
+const defaultGrade = (h) => {
+    return {
+        grades: [],
+        avg: 100,
+        name: 'New Grade',
+        heritage: h,
+        id: (new Date()).getTime()-1515215358101
+    }
+}
+
+const adding = (state, h, ogH) => {
+    if (state.id === h[0]) {
+        if (h.length > 1) {
+            return {...state,grades: state.grades.map(g => adding(g, h.slice(1), ogH))};
+        }
+        return {...state,grades: state.grades.concat(defaultGrade(ogH))};
+    }
+    return state;
+}
+
+const changingAvg = (state, h, avg) => {
+    if (state.id === h[0]) {
+        if (h.length > 1) {
+            return {...state,grades: state.grades.map(g => changingAvg(g, h.slice(1), avg))};
+        }
+        return { ...state,avg: avg};
+    }
+    return state;
+}
+
+const changingName = (state, h, name) => {
+    if (state.id === h[0]) {
+        if (h.length > 1) {
+            return {...state,grades: state.grades.map(g => changingName(g, h.slice(1), name))};
+        }
+        return { ...state,name: name};
+    }
+    return state;
+}
+
+const grade = (state, action) => {
     switch (action.type) {
-        case 'CHANGE':
-          return state + 1
+        case 'MORE':
+            return defaultGrade(action.h);
+        case 'ADD':
+            return adding(state, action.h, action.h);
+        case 'CHANGE_AVG':
+            return changingAvg(state, action.h, action.avg);
+        case 'CHANGE_NAME':
+            return changingName(state, action.h, action.name);
         default:
           return state
       }
 }
 
-const Grade = ({
-    value,
-    onChange
-}) => (
-    <div>
-      <p><strong>{value}</strong></p>
-      <ul>
-        <li><button className="btn btn-warning btn-sm" onClick={onChange}>Change</button></li>
-      </ul>
-    </div>
-)
+const grades = (state = [], action) => {
+    switch (action.type) {
+        case 'MORE':
+            return [...state,grade(undefined, action)];
+        case 'ADD':
+            return state.map(g => grade(g, action));
+        case 'CHANGE_AVG':
+            return state.map(g => grade(g, action));
+        case 'CHANGE_NAME':
+            return state.map(g => grade(g, action));
+        default:
+          return state;
+      }
+}
 
-const store = createStore(grade);
+const initialData = {
+    avg: 100,
+    name: 'Overall Grade',
+    id: (new Date()).getTime()
+}
+
+const baseData = (state = initialData, action) => {
+    switch (action.type) {
+        case 'MODIFY':
+            return state;
+        default:
+            return state;
+        }
+}
+
+const gradeApp = combineReducers({
+    grades,
+    baseData
+});
+
+const store = createStore(gradeApp);
 
 const render = () => {
     ReactDOM.render(
@@ -34,13 +181,8 @@ const render = () => {
             Work in progress, check back soon.
           </p>
           <div className="base-grade">
-              <Grade
-                value={store.getState()}
-                onChange={() =>
-                    store.dispatch({
-                        type: 'CHANGE'
-                    })
-                }
+              <GradeApp
+                state={store.getState()}
               />
           </div>
         </div>,
