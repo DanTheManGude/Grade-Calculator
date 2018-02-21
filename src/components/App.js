6bbb326ac0f0ef4b15e3d23f222d2526edc8d094
DanTheManGude <djg4755@rit.edu>
2018-02-21 09:51:59 -0500

    Added custom font and re organized stylesheet

--- src/components/App.js
+++ src/components/App.js
@@ -77,7 +77,7 @@ export class App extends React.Component {
                 <div className="container default">
                   <div className="row">
                     <div className="col-lg-12 intro">
-                      <h2 className="mt-5">Welcome to Grade Calculator</h2>
+                      <h2 id="title" className="mt-5">Welcome to Grade Calculator</h2>
                       <h5> A hassle free way to calculate your grade average.</h5>
                       {/*into blurb and a quick get started instructions*/}
                       <div className="alert alert-info alert-dismissable">
