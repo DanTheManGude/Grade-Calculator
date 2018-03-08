418473c3e74913c17405133803c9f05ab9435cd7
Daniel Gude <djg4755@rit.edu>
2018-03-08 02:04:17 -0500

    Created public config file

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: "AIzaSyCOALrrer0V_1X5apyP4VTlyCHU-nV10IM",
+    authDomain: "grade-calculator-public.firebaseapp.com",
+    databaseURL: "https://grade-calculator-public.firebaseio.com",
+    projectId: "grade-calculator-public",
+    storageBucket: "",
+    messagingSenderId: "232036583514"
+  };
